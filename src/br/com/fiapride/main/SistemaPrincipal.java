bd8244f0dbefa0725f3bd98737cae8be4c9f0585
Periato <rm562117@fiap.com.br>
2026-03-03 18:01:40 -0300

    SistemaPrincipal.java

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -12,16 +12,18 @@ public class SistemaPrincipal {
         Lapis lapis1 = new Lapis();
         lapis1.cor = "Amarelo";
         lapis1.tamanho = 12.0;
+        lapis1.pontaAfiada = true;
 
         // Criando o segundo lapis (Objeto 2)
         Lapis lapis2 = new Lapis();
         lapis2.cor = "Verde";
         lapis2.tamanho = 15.50;
+        lapis2.pontaAfiada = true;
 
         // Exibindo os dados no Console
         System.out.println("--- Sistema FiapRide ---");
-        System.out.println("Cor do lápis: " + lapis1.cor + " | Tamanho(cm): R$" + lapis1.tamanho);
-        System.out.println("Cor do lápis: " + lapis2.cor + " | Tamanho(cm): R$" + lapis2.tamanho);
+        System.out.println("Cor do lápis: " + lapis1.cor + " | Tamanho(cm): R$" + lapis1.tamanho + " | Ponta afiada" + lapis1.pontaAfiada);
+        System.out.println("Cor do lápis: " + lapis2.cor + " | Tamanho(cm): R$" + lapis2.tamanho + " | Ponta afiada" + lapis2.pontaAfiada);
         
     }
 }
