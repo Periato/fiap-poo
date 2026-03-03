d8d270f4d5587980e28d34b726c83a5821facac7
Periato <rm562117@fiap.com.br>
2026-03-03 17:57:11 -0300

    Lapis.java

--- src/br/com/fiapride/model/Lapis.java
+++ src/br/com/fiapride/model/Lapis.java
@@ -4,6 +4,24 @@ package br.com.fiapride.model;
 public class Lapis {
     // Atributos (Características)
     public String cor;
-    public double tamanho; 
+    public double tamanho;
+    public boolean pontaAfiada;
 
+public Lapis(String cor, boolean pontaAfiada) {
+    this.cor = cor;
+    this.pontaAfiada = false;
+    this.tamanho = 0.0;
+}
+
+public void apontarPonta() {
+    this.pontaAfiada = true;
+    System.out.println("A ponta está afiada");
+}
+
+public void desenhar() {
+    if (!cor.equals("grafite")) {
+        System.out.println("O lápis é de desenhar");
+        return;
+    }
+    System.out.println("O lápis é de escrever");
 }
