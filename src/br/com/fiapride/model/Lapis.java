9645e60b23ee9d557ca13bc2ea5c93863345623d
Periato <rm562117@fiap.com.br>
2026-03-06 22:08:42 -0300

    Lapis.java

--- src/br/com/fiapride/model/Lapis.java
+++ src/br/com/fiapride/model/Lapis.java
@@ -3,16 +3,43 @@ package br.com.fiapride.model;
 
 public class Lapis {
     // Atributos (Características)
-    public String cor;
-    public double tamanho;
-    public boolean pontaAfiada;
+    private String cor;
+    private double tamanho;
+    private boolean pontaAfiada;
 
 public Lapis(String cor, boolean pontaAfiada) {
     this.cor = cor;
     this.pontaAfiada = false;
     this.tamanho = 0.0;
 }
+public String getCor() {
+    return cor;
+}
 
+public double getTamanho() {
+    return tamanho;
+}
+
+public boolean isPontaAfiada() {
+    return pontaAfiada;
+}
+
+public void setCor(String cor) {
+    this.cor = cor;
+}
+
+public void setTamanho(double tamanho) {
+    if (tamanho < 0.0 || tamanho > 19.0) {
+        System.out.println("Tamanho inválido! O lápis deve ter entre 0 e 19 cm.");
+        return;
+    }
+    this.tamanho = tamanho;
+}
+
+public void setPontaAfiada(boolean pontaAfiada) {
+    this.pontaAfiada = pontaAfiada;
+}
+    
 public void apontarPonta() {
     this.pontaAfiada = true;
     System.out.println("A ponta está afiada");
